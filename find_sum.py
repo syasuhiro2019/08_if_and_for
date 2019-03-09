43d72d5f48ef027ae3a020e60bd35ea35f23ae2a
Sato <s_ychm@yahoo.co.jp>
2019-03-09 17:17:14 +0900

    find_max

--- find_sum.py
+++ find_sum.py
@@ -1,10 +1,10 @@
 numbers = [34, 432, 1, 99]
 
 total = 0
-total= total + numbers[0]
-total= total + numbers[1]
-total= total + numbers[2]
-total= total + numbers[3]
+total = total + numbers[0]
+total = total + numbers[1]
+total = total + numbers[2]
+total = total + numbers[3]
 
 print(total)
 
@@ -13,10 +13,11 @@ total = 0
 for number in numbers:
     total = total + number
 
-print (total)
+total = 0
+
+print(total)
 
 for number in numbers:
     total += number
 
 print(total)
-
